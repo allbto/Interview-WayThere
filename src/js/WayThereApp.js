8770972c8c7344ad548bd25f6dfb6a19179babed
allbto <allan.barbato@gmail.com>
2015-05-02 16:50:48 +0200

    Add: Primary source files

---
+++ src/js/WayThereApp.js
@@ -0,0 +1,15 @@
+/**
+ * Created by allan on 5/2/15.
+ */
+
+
+(function(ng) {
+    'use strict';
+
+    var app = ng.module("wayThereApp", ['ui.bootstrap']);
+
+    app.controller('WayThereController', function($scope) {
+
+    });
+
+})(angular);
