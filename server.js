cfb33e47a71965c1589c8d64e458611bb7917c9e
allbto <allan.barbato@gmail.com>
2015-05-02 19:12:10 +0200

    Add: Youtube embed player

--- server.js
+++ server.js
@@ -5,6 +5,11 @@
 var express = require('express');
 var app = express();
 
+//app.get('/*', function(req, res, next){
+//    res.setHeader('Last-Modified', (new Date()).toUTCString());
+//    next();
+//});
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/src/html/main.html');
 });
--- src/js/WayThereApp.js
+++ src/js/WayThereApp.js
@@ -6,10 +6,29 @@
 (function(ng) {
     'use strict';
 
-    var app = ng.module("wayThereApp", ['ui.bootstrap']);
+    var app = ng.module("wayThereApp", ['ui.bootstrap', 'youtube-embed']);
 
     app.controller('WayThereController', function($scope) {
 
+        //$scope.ytPlayer = {};
+        $scope.ytPlayerVars = {
+            controls: 0,
+            autoplay: 1,
+            disablekb : 1,
+            showinfo : 0,
+            hd : 1
+        };
+
+        $scope.currentYtId = '2G8LAiHSCAs';
+
+        $scope.$on('youtube.player.ready', function ($event, player) {
+            player.mute();
+        });
+
+        $scope.$on('youtube.player.ended', function ($event, player) {
+            // play it again when stoped
+            player.playVideo();
+        });
     });
 
 })(angular);
