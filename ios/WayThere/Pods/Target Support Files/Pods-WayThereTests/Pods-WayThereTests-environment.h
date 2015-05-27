af8ee1e1c9d9eb285d17a23d4660d1b320d7f0b9
allbto <allan.barbato@gmail.com>
2015-05-27 22:57:09 +0200

    Add: Pods as requested in the guidelines
    I would need to know if it’s really the best practice, I saw many
    different opinion on internet while doing the project. Anyway here is
    it.

---
+++ ios/WayThere/Pods/Target Support Files/Pods-WayThereTests/Pods-WayThereTests-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// MagicalRecord
+#define COCOAPODS_POD_AVAILABLE_MagicalRecord
+#define COCOAPODS_VERSION_MAJOR_MagicalRecord 2
+#define COCOAPODS_VERSION_MINOR_MagicalRecord 2
+#define COCOAPODS_VERSION_PATCH_MagicalRecord 0
+
+// MagicalRecord/Core
+#define COCOAPODS_POD_AVAILABLE_MagicalRecord_Core
+#define COCOAPODS_VERSION_MAJOR_MagicalRecord_Core 2
+#define COCOAPODS_VERSION_MINOR_MagicalRecord_Core 2
+#define COCOAPODS_VERSION_PATCH_MagicalRecord_Core 0
+
+// Nimble
+#define COCOAPODS_POD_AVAILABLE_Nimble
+#define COCOAPODS_VERSION_MAJOR_Nimble 0
+#define COCOAPODS_VERSION_MINOR_Nimble 4
+#define COCOAPODS_VERSION_PATCH_Nimble 2
+
